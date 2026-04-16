af3a3b5ec9fbdd6f22b2c3b1f6931294898e1912
Harsha-Nimmagadda-eng <snimma13@asu.edu>
2026-04-16 16:32:32 -0700

    Update hero-data.js

--- assets/hero-data.js
+++ assets/hero-data.js
@@ -1,5 +1,5 @@
 window.__HERO__ = {
   title: "Hello, I am Harsha Nimmagadda",
   role: "Computer Engineering ( Electrical Engineering ) / ASIC / ML Engineer",
-  blurb: "Focused on ASIC design, DFT, and machine learning for hardware systems, with strong interest in semiconductor research and scalable engineering workflows."
+  blurb: "Focused on ASIC design, Circuit design, Edge IOT systems, Reinforcement learning and machine learning for hardware systems, with strong interest in semiconductor research and scalable engineering workflows."
 };
