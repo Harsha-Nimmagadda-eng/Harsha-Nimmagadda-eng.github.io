618bc7ca957ddeeba88361f57966ad4ac58d5b62
Sai Harsha Nimmagadda (Student) <snimma13@sundevils.asu.edu>
2026-04-16 16:02:34 -0700

    Add editable projects-data file and load it before bundle

---
+++ assets/projects-data.js
@@ -0,0 +1,61 @@
+window.__PROJECTS__ = [{
+  title: "zERO TOUCH WIFI ENABLED SENSORS",
+  desc: "Task Mate is a Web Application that helps users manage their daily tasks and study materials efficiently. It leverages the power of OpenAI to generate personalized study plans and materials based on user inputs and Users can store and access their study materials in a user-friendly interface.",
+  subdesc: "Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Firebase , Task Mate is designed for optimal performance and scalability.",
+  href: "https://taskmate-007.vercel.app/",
+  texture: "/textures/project/Project_1.mp4",
+  logo: "/assets/project-logo1.png",
+  logoStyle: {
+    backgroundColor: "#2A1816",
+    border: "0.2px solid #36201D",
+    boxShadow: "0px 0px 60px 0px #AA3C304D"
+  },
+  spotlight: "/assets/spotlight1.png",
+  tags: [{
+    id: 1,
+    name: "PYTHON",
+    path: "/assets/react.svg"
+  }, {
+    id: 2,
+    name: "RTL",
+    path: "assets/tailwindcss.png"
+  }, {
+    id: 3,
+    name: "TypeScript",
+    path: "/assets/typescript.png"
+  }, {
+    id: 4,
+    name: "Framer Motion",
+    path: "/assets/framer.png"
+  }]
+}, {
+  title: "UART",
+  desc: "JARVIS is a powerful Chatbot that can act as a Personal Assistant and Study Mentor with Voice Assistant..",
+  subdesc: "With JARVIS, User can chat with the bot and ask about their acadamic needs and user can also upload materials and the bot will give you the summary of the particular file. and it has also voice feature that can be used to interact with the bot.",
+  href: "https://jarvis-ten-sage.vercel.app/",
+  texture: "/textures/project/Project_2.mp4",
+  logo: "/assets/project-logo2.png",
+  logoStyle: {
+    backgroundColor: "#13202F",
+    border: "0.2px solid #17293E",
+    boxShadow: "0px 0px 60px 0px #2F6DB54D"
+  },
+  spotlight: "/assets/spotlight2.png",
+  tags: [{
+    id: 1,
+    name: "PYTHON",
+    path: "/assets/react.svg"
+  }, {
+    id: 2,
+    name: "RTL",
+    path: "assets/tailwindcss.png"
+  }, {
+    id: 3,
+    name: "TypeScript",
+    path: "/assets/typescript.png"
+  }, {
+    id: 4,
+    name: "Framer Motion",
+    path: "/assets/framer.png"
+  }]
+}];
